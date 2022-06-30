14630c65e0202ae024815deae826f1cec89ac39c
Myrotvorets <support@myrotvorets.center>
2022-06-30 21:09:12 +0300

    First commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * Plugin Name: Cloudflare Helper
+ * Plugin URI: https://myrotvorets.center/
+ * Version: 1.0.0
+ * Author: Myrotvorets
+ * Author URI: https://myrotvorets.center/
+ * License: MIT
+ */
+
+use Myrotvorets\WordPress\CloudflareHelper\Plugin;
+
+if ( defined( 'ABSPATH' ) ) {
+	if ( defined( 'VENDOR_PATH' ) ) {
+		/** @psalm-suppress UnresolvableInclude, MixedOperand */
+		require constant( 'VENDOR_PATH' ) . '/vendor/autoload.php'; // NOSONAR
+	} elseif ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+		require __DIR__ . '/vendor/autoload.php';
+	} elseif ( file_exists( ABSPATH . 'vendor/autoload.php' ) ) {
+		/** @psalm-suppress UnresolvableInclude */
+		require ABSPATH . 'vendor/autoload.php';
+	}
+
+	Plugin::instance();
+}
