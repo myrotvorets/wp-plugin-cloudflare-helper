217262f007a68702580d77305a3f2b5c84730678
Myrotvorets <support@myrotvorets.center>
2024-02-11 08:18:25 +0200

    Drop VENDOR_PATH

--- index.php
+++ index.php
@@ -12,10 +12,7 @@
 use Myrotvorets\WordPress\CloudflareHelper\Plugin;
 
 if ( defined( 'ABSPATH' ) ) {
-	if ( defined( 'VENDOR_PATH' ) ) {
-		/** @psalm-suppress UnresolvableInclude, MixedOperand */
-		require_once constant( 'VENDOR_PATH' ) . '/vendor/autoload.php'; // NOSONAR
-	} elseif ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+	if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
 		require_once __DIR__ . '/vendor/autoload.php';
 	} elseif ( file_exists( ABSPATH . 'vendor/autoload.php' ) ) {
 		require_once ABSPATH . 'vendor/autoload.php';
